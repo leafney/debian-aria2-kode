04c9a4ec8ed4aad01ea55668f6213e29038d3afd
tozehu <tozehu@126.com>
2019-04-25 10:44:04 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,6 @@
 set -e
 
 # env
-RPC_SECRET=${RPC_SECRET:-"123456"}
 
 echo "***** set php web config *****"
 
@@ -20,24 +19,12 @@ cat << EOF > $apache2_conf
 	</Directory>
 </VirtualHost>
 
-<VirtualHost *:${ARIANG_PORT}>
-	ServerAdmin webmaster2@localhost
-	DocumentRoot /web/ariaNg
-	ErrorLog ${APACHE_LOG_DIR}/error.log
-	CustomLog ${APACHE_LOG_DIR}/access.log combined
-	<Directory /web/ariaNg/>
-		Options Indexes FollowSymLinks
-		AllowOverride None
-		Require all granted
-	</Directory>
-</VirtualHost>
 EOF
 
 echo "***** set php web port *****"
 port_conf="/etc/apache2/ports.conf"
 cat << EOF > $port_conf
 Listen ${KODE_PORT}
-Listen ${ARIANG_PORT}
 
 <IfModule ssl_module>
         Listen 443
@@ -48,36 +35,9 @@ Listen ${ARIANG_PORT}
 </IfModule>
 EOF
 
-aria2_conf="/app/conf/aria2.conf"
-aria2_session="/app/conf/aria2.session"
-
-if [ -f "$aria2_conf" ]; then
-	echo "***** Loading aria2 config *****"
-else
-	echo "***** Creating default aria2 config *****"
-	mkdir -p /app/conf
-
-	cat << EOF >> $aria2_conf
-dir=/app/aria2down
-continue=true
-input-file=/app/conf/aria2.session
-save-session=/app/conf/aria2.session
-disable-ipv6=true
-enable-rpc=true
-rpc-allow-origin-all=true
-rpc-listen-all=true
-rpc-listen-port=$RPC_PORT
-rpc-secret=$RPC_SECRET
-EOF
 
 fi
 
-if [ -f "$aria2_session" ]; then
-	echo "***** Loading aria2 session file *****"
-else
-	echo "***** Creating default aria2 session file *****"
-	touch $aria2_session
-fi
 
 mkdir -p /app/aria2down
 mkdir -p /app/logs
